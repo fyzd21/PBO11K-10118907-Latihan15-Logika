6f97ddee6dab5eaa07b1b33f007618dabc6ebcca
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:33:05 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan15/logika/PBO11K10118907Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan15.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan logika
+ * 
+ */
+public class PBO11K10118907Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      boolean a =true;
+      boolean b = false;
+      System.out.println("a && b = "+(a && b));
+      System.out.println("a || b = "+(a || b));
+      System.out.println("!a = "+(!a));
+      System.out.println("! (a && b) = "+!(a && b));
+    }
+    
+}
